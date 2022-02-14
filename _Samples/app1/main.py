2d253d98a9e966e02f727a3ad84bd99aefeb6e3e
chempik1234 <dandijar@yandex.ru>
2022-02-14 18:50:21 +0500

    third

---
+++ _Samples/app1/main.py
@@ -0,0 +1,37 @@
+from flask import Flask
+from _Samples.app1.data import db_session
+from _Samples.app1.data.__all_models import *
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+def main():
+    amount = 6
+    surnames = ["Scott", "Jackson", "Kamado", "Carry", "Brown", "Freeman"]
+    names = ["Riddley", "Michael", "Manfred", "Ben", "John", "Albert"]
+    ages = [21, 23, 34, 29, 38, 25]
+    positions = ["captain", "private", "corporal", "private first class", "staff-sergeant", "private"]
+    specialities = ["research engineer", "research engineer", "life support engineer",
+                    "drone pilot", "medic", "builder"]
+    addresses = ["module 1", "module 1", "module 2", "module 3", "module 2", "module 3"]
+    emails = ["scott_chief@mars.org", "michael_singer@mars.org", "foreigner_guy@mars.org",
+              "arma_III@mars.org", "stereotypical_name@mars.org", "science4life@mars.org"]
+    db_sess = db_session.create_session()
+    for i in range(6):
+        user = User()
+        user.surname = surnames[i]
+        user.name = names[i]
+        user.age = ages[i]
+        user.position = positions[i]
+        user.speciality = specialities[i]
+        user.address = addresses[i]
+        user.email = emails[i]
+        db_sess.add(user)
+    db_sess.commit()
+    #app.run()
+
+
+if __name__ == '__main__':
+    db_session.global_init("db/mars_explorer.db")
+    main()
